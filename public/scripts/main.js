3c880d44bd6a141e36d5ff0e949ccda980c82bd1
offthegame <ceokingmaker@naver.com>
2025-02-22 22:27:58 +0900

    sound play 2

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -10,7 +10,7 @@ import {
   updateQuestionUI 
 } from "/scripts/ui.js";
 
-import { playBackgroundMusic, playButtonSound, playHitSound, playWrongSound, updateScoreUI, updateLivesUI, updateTimerUI, updateQuestionUI } from "/scripts/sound.js";
+import { playBackgroundMusic, playButtonSound, playHitSound, playWrongSound } from "/scripts/sound.js";
 
 // ======================
 // 2. Global Variables & Game State
